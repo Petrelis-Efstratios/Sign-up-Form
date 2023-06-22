225569d84194a088ea8c25df129238e27181393e
Petrelis-Efstratios <petrelis-stratos@outlook.com.gr>
2023-06-22 23:16:25 +0300

    Added dark mode

---
+++ script.js
@@ -0,0 +1,37 @@
+const themeIcon = document.querySelector(".theme_button > img");
+const themeButton = document.querySelector(".theme_button");
+let darkModeEnabled = false;
+if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    darkMode();
+}
+function darkMode() {
+    themeIcon.src = "images/dark_mode.svg";
+    document.documentElement.style.cssText = "--text-color: white;" +
+    "--background-color-1: #2a040e;" +
+    "--background-color-2: #4d0719;" +
+    "--shadow-1: rgba(180, 180, 180, 0.3);" +
+    "--shadow-2: white;" +
+    "--border-1: black;" +
+    "--border-2: #ced6e9;";
+    darkModeEnabled = true
+}
+function lightMode() {
+    themeIcon.src = "images/light_mode.svg";
+    document.documentElement.style.cssText = "--text-color: black;" +
+    "--background-color-1 = #f9fafb" +
+    "--background-color-2: white;" +
+    "--shadow-1: rgb(180, 180, 180);" +
+    "--shadow-2: grey;" +
+    "--border-1: #E5E7EB;" +
+    "--border-2: black;";
+    darkModeEnabled = false;
+}
+themeButton.addEventListener("click", function() {
+    switch(darkModeEnabled) {
+        case true:
+            lightMode();
+            break;
+        default:
+            darkMode();
+    }
+})
